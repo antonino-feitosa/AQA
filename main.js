a685596ca88acdbb1ac082656e2dddc646fa09ac
Antonino Feitosa <antonino_feitosa@yahoo.com.br>
2022-11-25 22:08:19 +0000

    Add short and long questions

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 class AQA {
 	constructor() {
-		this.text = "<table><tr>\n";
+		this.text = "<table>\n";
+		this.answer = new Map();
+		this.difficulty = new Map();
+		this.idCount = 0;
 	}
 
 	setTile(title, theory){
 		let textTitle = "<summary>"+title+"</summary>";
-		let textTheory = "<details>"+ textTitle + theory + "</details>\n";
+		let textTheory = "<details>"+ textTitle + theory + "</details><br>";
 		this.text += textTheory + this.text;
 	}
 
-	addDescription() {
-
+	_addCell(text){
+		this.text += "<tr><td>"+text+"</td></tr>";
 	}
 
-	addQuestionShort(){
+	_insertQuestion(q){
+		let id = this.idCount++;
+		this.answer.set(id, q.answer);
+		this.difficulty.set(id, q.difficulty);
+		return id;
+	}
+
+	addDescription(desc) {
+		this._addCell(desc.question);
+	}
+
+	addQuestionShort(q){
+		let id = this._insertQuestion(q);
+		let text = "<br>" + q.question + "<br>";
+		text += "<input type=\"text\" id=\""+ id +"\"><br>"
+		this._addCell(text);
 		
 	}
 
-	addQuestionLong(){
-		
+	addQuestionLong(q){
+		let id = this._insertQuestion(q);
+		let text = "<br>" + q.question + "<br>";
+		text += "<textarea id=\""+id+"\"></textarea><br>"
+		this._addCell(text);
 	}
 
 	addQuestionChoice(){
@@ -29,7 +50,7 @@ class AQA {
 	}
 
 	display(){
-		this.text += "\n</tr></table>";
+		this.text += "\n</table>";
 		document.body.innerHTML = this.text;
 	}
 
@@ -41,6 +62,16 @@ class AQA {
 		const response = await fetch(fileName);
 		const json = await response.json();
 		this.setTile(json.title, json.theory);
+		for(let q of json.questions){
+			switch(q.type){
+				case "description":
+					this.addDescription(q); break;
+				case "short":
+					this.addQuestionShort(q); break;
+				case "long":
+					this.addQuestionLong(q); break;
+			}
+		}
 		this.display();
 	}
 }
